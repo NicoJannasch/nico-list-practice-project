8c4860894b7fee3ea6203f91256af87a07836a20
NicoJannasch <travelsourcellc@gmail.com>
2021-06-21 15:16:31 +0200

    No need for full page reload now

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const button = document.querySelector(`.button`);
 const buttonClear = document.querySelector(`.button_clear`);
-const body = document.querySelector(`body`);
 const content = document.querySelector(`.content`);
 
 let counter = 1;
@@ -12,9 +11,26 @@ button.addEventListener(`click`, function () {
     `<h2 class="text text_${counter}">${text}</h2>`
   );
   localStorage.setItem(`listItem ${counter}`, text);
+  const currentListItemName = `listItem ${counter}`
+//   console.log(currentListItemName)
   localStorage.setItem(`counter`, `${counter}`);
+  
+  //EXPERIMENTAL NEW CODE//////
+  let currentText = document.querySelector(`.text_${counter}`)
+  
+  currentText.addEventListener(`click`, function (e) {
+      console.log(`Clicked!`, counter , e.target.textContent)
+      e.target.textContent.slice(-1) != `✅`
+        ? (e.target.textContent += ` ✅`)
+        : (e.target.textContent = e.target.textContent.slice(0, -2));
+        localStorage.setItem(currentListItemName, e.target.textContent);
+  })
+
+  console.log(counter)
   counter++;
-  location.reload(); //TODO: Needed due to double-eventlistener error (remove full page reload)
+  console.log(counter)
+    //Working Code That Reloads Page Fully - Use If Necessary//
+//   location.reload(); //TODO: Needed due to double-eventlistener error (remove full page reload)
 });
 
 buttonClear.addEventListener(`click`, function () {
@@ -36,10 +52,10 @@ window.addEventListener(`load`, function () {
       `<h2 class="text">${localStorageItem}</h2>`
     );
   }
-  addEventListenerToText();
+  addEventListenerToAllTextElements();
 });
 
-const addEventListenerToText = function () {
+const addEventListenerToAllTextElements = function () {
   let textElements = document.getElementsByClassName(`text`);
   for (let i = 0; i < textElements.length; i++) {
     console.log(i, textElements[i]);
